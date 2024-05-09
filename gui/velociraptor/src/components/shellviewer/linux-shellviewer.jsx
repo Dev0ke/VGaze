af42c4e3d49424b4e533ce64c4a3d7a2d9840878
Snow <68282250+ZongaoHuang@users.noreply.github.com>
2024-05-09 13:38:16 +0800

    Refactor shell viewer component and add open terminal button

--- gui/velociraptor/src/components/shellviewer/linux-shellviewer.jsx
+++ gui/velociraptor/src/components/shellviewer/linux-shellviewer.jsx
@@ -1,55 +1,61 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Terminal } from 'xterm';
 import 'xterm/css/xterm.css';
-import { useParams } from 'react-router-dom';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import HexView from '../utils/hex.jsx';
 
-function TerminalComponent() {
-    const { filePath } = useParams();
+function TerminalComponent({ base64Content }) {
     const terminalRef = useRef(null);
+    const [show, setShow] = useState(false);
     const [terminal, setTerminal] = useState(null);
-    const [show, setShow] = useState(true);
 
     useEffect(() => {
-        const newTerminal = new Terminal({
-            cursorBlink: true,
-            theme: {
-                background: '#202B33', // Ensure background and text colors are set to see the contrast
-                foreground: '#F5F8FA'
-            }
-        });
-        newTerminal.open(terminalRef.current);
-        setTerminal(newTerminal);
-
-        return () => newTerminal.dispose();
-    }, []);
-
-    useEffect(() => {
-        if (terminal) {
-            fetch(`/${filePath}`)
-                .then(response => response.text())
-                .then(text => {
-                    terminal.write(text);
-                })
-                .catch(err => console.error('Failed to load log:', err));
+        if (!terminal) {
+            const newTerminal = new Terminal({
+                cursorBlink: true,
+                theme: {
+                    background: '#202B33',
+                    foreground: '#F5F8FA'
+                }
+            });
+            newTerminal.open(terminalRef.current);
+            setTerminal(newTerminal);
         }
-    }, [terminal, filePath]);
 
+        return () => {
+            terminal?.dispose();
+        };
+    }, [terminal]);
+
+    useEffect(() => {
+        if (terminal && base64Content) {
+            const decodedData = atob(base64Content);  // 解码 base64
+            terminal.write(decodedData);
+        }
+    }, [terminal, base64Content]);
+
+    const handleOpen = () => setShow(true);
     const handleClose = () => setShow(false);
 
     return (
-        <Modal show={show} onHide={handleClose} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
-            <Modal.Header closeButton>
-                <Modal.Title>Terminal Logs</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div ref={terminalRef} style={{ height: "400px", width: "100%" }}></div>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={handleClose}>Close</Button>
-            </Modal.Footer>
-        </Modal>
+        <>
+            <Button onClick={handleOpen} className="hex-popup client-link" variant="outline-info">
+                <FontAwesomeIcon icon="external-link-alt"/> Open Terminal
+            </Button>
+            <Modal show={show} onHide={handleClose} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Terminal Logs</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div ref={terminalRef} style={{ height: "400px", width: "100%" }}></div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={handleClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
 
